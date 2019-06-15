46129eb6d1683d96d1f74e269f10e4d2b3f30869
Jinwoo Kim <ypk02007@naver.com>
2019-06-15 16:44:17 +0900

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ import java.awt.event.*;
 import java.util.HashMap;
 
 public class Main extends JFrame {
-	private Container c = null;
-	private JLabel background = null;
+	private JPanel board = null;
+	private ImageIcon background = null;
 	private BGMPlayer bgm = null;
 	private HashMap<String, JLabel> sprites = new HashMap<String, JLabel>();
 	
@@ -13,18 +13,24 @@ public class Main extends JFrame {
 		setTitle("분노의 노란공");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setIconImage(new ImageIcon("graphics/icon.png").getImage());
-		setSize(650, 730);
+		setSize(655, 730);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        int mw = dim.width / 2 - 328;
+        int mh = (int) (dim.height * 0.05f);
+        setLocation(mw, mh);
 		setResizable(false);
 		
-		c = getContentPane();
-		c.setLayout(null);
+		background = new ImageIcon("graphics/bg/title.png");
 		
-		background = new JLabel(new ImageIcon("graphics/bg/title.png"));
-		background.setSize(650, 700);
-		background.setLocation(0, 0);
-		c.add(background);
+		board = new JPanel() {
+			public void paintComponent(Graphics g) {
+				g.drawImage(background.getImage(), 0, 0, null);
+			}
+		};
+		board.setLayout(null);
+		setContentPane(board);
 		
-		setSelectIcons();
+		setSprites();
 		
 		bgm = new BGMPlayer(0);
 		bgm.loop();
@@ -35,13 +41,14 @@ public class Main extends JFrame {
 		addKeyListener(new Controller());
 	}
 	
-	public void setSelectIcons() { // 적 선택에 관련된 JLabel들을 세팅
+	public void setSprites() { // 정적인 그래픽 요소의 JLabel들을 세팅
 		JLabel face1 = new JLabel(new ImageIcon("graphics/face/enemy1.png"));
 		JLabel face2 = new JLabel(new ImageIcon("graphics/face/enemy2.png"));
 		JLabel face3 = new JLabel(new ImageIcon("graphics/face/enemy3.png"));
 		JLabel face4 = new JLabel(new ImageIcon("graphics/face/enemy4.png"));
 		JLabel select = new JLabel(new ImageIcon("graphics/select.png"));
 		JLabel intro = new JLabel(new ImageIcon());
+		JLabel frame = new JLabel(new ImageIcon("graphics/frame.png"));
 		int horizon = 450;
 		int gap = 150;
 		
@@ -51,19 +58,22 @@ public class Main extends JFrame {
 		face4.setSize(100, 100);
 		select.setSize(110, 110);
 		intro.setSize(550, 300);
+		frame.setSize(650, 700);
 		
 		face1.setLocation(50, horizon);
 		face2.setLocation(50 + gap * 1, horizon);
 		face3.setLocation(50 + gap * 2, horizon);
 		face4.setLocation(50 + gap * 3, horizon);
 		intro.setLocation(50, 100);
+		frame.setLocation(0, 0);
 		
-		background.add(face1);
-		background.add(face2);
-		background.add(face3);
-		background.add(face4);
-		background.add(select);
-		background.add(intro);
+		board.add(face1);
+		board.add(face2);
+		board.add(face3);
+		board.add(face4);
+		board.add(select);
+		board.add(intro);
+		board.add(frame);
 		
 		face1.addMouseListener(new SelectEnemyEvent());
 		face2.addMouseListener(new SelectEnemyEvent());
@@ -76,6 +86,7 @@ public class Main extends JFrame {
 		face4.setVisible(false);
 		select.setVisible(false);
 		intro.setVisible(false);
+		frame.setVisible(false);
 		
 		sprites.put("face1", face1);
 		sprites.put("face2", face2);
@@ -83,6 +94,7 @@ public class Main extends JFrame {
 		sprites.put("face4", face4);
 		sprites.put("select", select);
 		sprites.put("intro", intro);
+		sprites.put("frame", frame);
 	}
 	
 	public void readyForBattle(int backgroundCode) { // 적과의 전투를 준비, 파라미터는 backgroundCode이다
@@ -92,8 +104,69 @@ public class Main extends JFrame {
 		sprites.get("face4").setVisible(false);
 		sprites.get("select").setVisible(false);
 		sprites.get("intro").setVisible(false);
+		sprites.get("frame").setVisible(true);
 		Config.getInstance().setBackgroundCode(backgroundCode);
 		changeBackground(Config.getInstance().getBackgroundCode());
+		ReadyGo rg = new ReadyGo();
+		rg.start();
+	}
+	
+	public void changeBackground(int code) { // 배경 이미지 전환
+		switch(code) {
+		case 0:
+			background = new ImageIcon("graphics/bg/title.png");
+			board.repaint();
+			break;
+		case 1:
+			background = new ImageIcon("graphics/bg/select.png");
+			board.repaint();
+			sprites.get("face1").setVisible(true);
+			sprites.get("face2").setVisible(true);
+			sprites.get("face3").setVisible(true);
+			sprites.get("face4").setVisible(true);
+			break;
+		case 2:
+			background = new ImageIcon("graphics/bg/bg1.png");
+			board.repaint();
+			break;
+		case 3:
+			background = new ImageIcon("graphics/bg/bg2.png");
+			board.repaint();
+			break;
+		case 4:
+			background = new ImageIcon("graphics/bg/bg3.png");
+			board.repaint();
+			break;
+		}
+	}
+	
+	public void handleError(String msg) { // 오류 처리
+		System.out.println(msg);
+		System.exit(1);
+	}
+	
+	class ReadyGo extends Thread { // READY와 GO!를 다이나믹하게 표시
+		public void run() {
+			JLabel jl = new JLabel(new ImageIcon("graphics/intro/ready.png"));
+			jl.setSize(400, 150);
+			jl.setLocation(0, 250);
+			board.add(jl);
+			
+			try {
+				sleep(20);
+				jl.setLocation(75, 250);
+				sleep(20);
+				jl.setLocation(125, 250);
+				sleep(20);
+				jl.setLocation(150, 250);
+				sleep(200);
+				board.remove(jl);
+				board.revalidate();
+				board.repaint();
+			} catch (InterruptedException e) {
+				handleError(e.getMessage());
+			}
+		}
 	}
 	
 	class SelectEnemyEvent extends MouseAdapter { // 적 선택과 관련된 마우스 이벤트들
@@ -157,30 +230,6 @@ public class Main extends JFrame {
 					break;
 				}
 			}
-		}
-	}
-	
-	public void changeBackground(int code) { // 배경 이미지 전환
-		switch(code) {
-		case 0:
-			background.setIcon(new ImageIcon("graphics/bg/title.png"));
-			break;
-		case 1:
-			background.setIcon(new ImageIcon("graphics/bg/select.png"));
-			sprites.get("face1").setVisible(true);
-			sprites.get("face2").setVisible(true);
-			sprites.get("face3").setVisible(true);
-			sprites.get("face4").setVisible(true);
-			break;
-		case 2:
-			background.setIcon(new ImageIcon("graphics/bg/bg1.png"));
-			break;
-		case 3:
-			background.setIcon(new ImageIcon("graphics/bg/bg2.png"));
-			break;
-		case 4:
-			background.setIcon(new ImageIcon("graphics/bg/bg3.png"));
-			break;
 		}
 	}
 	
